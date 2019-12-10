6c77c0d00872f42e50a0fe20d56287056467d17c
Cristian Gonzalez <crdgonzalezca@unal.edu.co>
2019-12-10 16:41:22 -0500

    Fix consumer

--- app/controllers/soapconsumer_controller.rb
+++ app/controllers/soapconsumer_controller.rb
@@ -6,7 +6,12 @@ class SoapconsumerController < ApplicationController
   
   def respuesta
     id = "#{params[:id]}"
-    
-    @user = User.new(id)
+    client = Savon::Client.new(wsdl: "http://35.238.81.164:5003/wsusers/wsdl")
+    response = client.call :check_user, message: { "userId" => id }
+    if response.success?
+        data = response.body[:check_user_response]
+        @user = data
+        render :json => data
+    end
   end
 end
